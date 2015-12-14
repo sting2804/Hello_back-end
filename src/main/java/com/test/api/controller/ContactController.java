95f654486885a9f187404331a4859ebcbbc995ed
sting2804 <slinnik284@gmail.com>
2015-12-14 23:08:13 +0200

    project initialization

---
+++ src/main/java/com/test/api/controller/ContactController.java
@@ -0,0 +1,11 @@
+package com.test.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sting on 12/14/15.
+ */
+@RestController
+public class ContactController {
+
+}
